af0481791871de62c08f74497de03392445854ac
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-08-23 12:04:14 +0300

    new

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,21 @@
-'use strict';
+"use strict";
 
 const profile = {
-    username: "Jacob",
-    playTime: 300,
+  username: "Jacob",
+  playTime: 300,
 
-    changeUsername(newName) {
-        this.username = newName;
-    },
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-    updatePlayTime(hours) {
-        this.playTime += hours;
-    },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
-    }
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
-
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
